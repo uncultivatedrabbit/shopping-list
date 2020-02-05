ee61b1ae9a5c60fb358990a26efc530a7686538b
Brandon Melendez <brandonmelendezdev@gmail.com>
2020-02-05 18:28:56 -0500

    Finished shopping list exercise

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Shopping List App Challenge
 
 This repo contains starter files for the *Shopping List App* challenge.
+
+
+In terms of user experience, your shopping list app must allow users to:
+
+<!-- 1) enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button -->
+
+<!-- 2) check and uncheck items on the list by clicking the "Check" button
+
+3) permanently remove items from the list -->
+
+----------------------------------------------------------------------
+
+Additionally:
+
+<!-- 4) You must use a CDN-hosted version of jQuery -->
+
+<!-- 5) Put your application code in a file called index.js and link to it in index.html -->
+
+<!-- 6) Be sure to put both script elements at the bottom of the <body> element.
+
+7) Do not alter index.html or main.css other than adding the links to the external JavaScript inside index.html. Write JavaScript code that works with the existing HTML and CSS to implement the required features. -->
+
+
+Hint: you may find it helpful to read up on and use the following jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
+When you're done, push your work up to GitHub and publish it on GitHub Pages. Submit a link to your repo below.
---
+++ index.js
@@ -0,0 +1,47 @@
+// document.ready function to ensure DOM is finished rendering
+$(() => {
+  // function that accounts for submitting by clicking the 'Add item" button or by pressing 'Enter'
+  $("#js-shopping-list-form").submit(e => {
+    // stop the form from refreshing the page
+    e.preventDefault();
+    // get the user input from the form
+    let input = $("#shopping-list-entry").val();
+    // verifies the user has actually put something in the shopping list box
+    if (input.length < 1) {
+      alert("Input field can't be empty!");
+      return -1;
+    }
+    // if there is something in the input box, adds new item to DOM shopping list
+    $(".shopping-list").append(`
+        <li>
+          <span class="shopping-item">${input}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>
+  `);
+    // clears the shopping list input value so the user can put in a new item
+    $("#shopping-list-entry").val("");
+  });
+
+  // function that uses event deligation to put an event listener on the shopping list
+  $(".shopping-list").on("click", ".shopping-item-toggle", function(e) {
+    // uses this instead of e.currentTarget then traverses the DOM tree to toggle class on specific shopping item
+    $(this)
+      .closest(".shopping-item-controls")
+      .siblings(".shopping-item")
+      .toggleClass("shopping-item__checked");
+  });
+
+  // function that removes an item from the shopping list
+  $(".shopping-list").on("click", ".shopping-item-delete", function(e) {
+    $(this)
+      .closest("li")
+      .remove();
+  });
+});
